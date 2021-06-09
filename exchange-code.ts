3a2bde12a3327f28d3c4779f8eb8e23322a7d071
s4nt14go <santiagopergamo@gmail.com>
2021-06-09 08:03:03 -0300

    feat: use SSM instead of .env file to access Civic keys
    Use custom.stage to manage multiple stages
    Add iam-roles-per-function & pseudo-parameters plugins
    Enable aws-sdk connection reuse

--- exchange-code.ts
+++ exchange-code.ts
@@ -1,23 +1,35 @@
 import { Context, APIGatewayProxyHandler, APIGatewayEvent } from "aws-lambda";
 import { wrapper } from './lib';
+import { SSM } from 'aws-sdk';
 import civicSip from 'civic-sip-api';
 
 const {
-  APP_ID,
-  PRV_KEY,
-  APP_SECRET,
+  serviceName,
+  stage,
 } = process.env;
 
-// Initialize instance passing your appId and secret.
-const civicClient = civicSip.newClient({
-  appId: APP_ID,
-  prvKey: PRV_KEY,
-  appSecret: APP_SECRET,
-});
+const ssm = new SSM();
 
 export const handler: APIGatewayProxyHandler = wrapper(async (event: APIGatewayEvent, _context: Context) => {
 
   console.log('event:', JSON.stringify(event, null, 2));
+
+  const Names = [
+    `/${serviceName}/${stage}/APP_ID`,
+    `/${serviceName}/${stage}/PRV_KEY`,
+    `/${serviceName}/${stage}/APP_SECRET`,
+  ]
+  const request = await ssm.getParameters({ Names }).promise();
+  const APP_ID = request.Parameters.filter(p => p.Name === Names[0])[0].Value;
+  const PRV_KEY = request.Parameters.filter(p => p.Name === Names[1])[0].Value;
+  const APP_SECRET = request.Parameters.filter(p => p.Name === Names[2])[0].Value;
+
+  // Initialize instance passing your appId and secret.
+  const civicClient = civicSip.newClient({
+    appId: APP_ID,
+    prvKey: PRV_KEY,
+    appSecret: APP_SECRET,
+  });
 
   // Exchange authorization code for user data.
   const token = event.body;
